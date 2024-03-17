51cd436b6ea5cd2601cb071c319ab88ad2e50f4c
DmitriyK2001 <112119664+DmitriyK2001@users.noreply.github.com>
2024-03-17 23:22:12 +0300

    Update train.py

--- music/train.py
+++ music/train.py
@@ -1,6 +1,26 @@
 
-def train_model():
-  return
+def train_model(model, iterator, optimizer, criterion, clip, train_history=None, valid_history=None):
+    model.train()
+
+    epoch_loss = 0
+    history = []
+    for i, batch in enumerate(iterator):
+        src = batch.src
+        trg = batch.trg
+        optimizer.zero_grad()
+        output = model(src, trg)
+        output = output[1:].view(-1, output.shape[-1])
+        trg = trg[1:].view(-1)
+        loss = criterion(output, trg)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+        optimizer.step()
+        epoch_loss += loss.item()
+
+        history.append(loss.cpu().data.numpy())
+
+
+    return epoch_loss / len(iterator)
 
 def train():
   return
