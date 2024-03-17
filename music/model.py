396b5550c933890207e28624507246c52550f5c3
DmitriyK2001 <112119664+DmitriyK2001@users.noreply.github.com>
2024-03-18 00:04:25 +0300

    Update model.py

--- music/model.py
+++ music/model.py
@@ -4,6 +4,10 @@ from torch import nn
 class model(nn.Module):
    def __init__(self, hid_dim):
       super().__init__()
-      self.linear = nn.Linear(hid_dim)   
-  def forward():
-        return self.linear
+      self.linear = nn.Linear(hid_dim, hid_dim)
+      self.act = nn.ReLU()
+  def forward(self, x):
+     for i in range(num_layers):
+        x = self.linear(x)
+        x = self.act(x)
+     return x
