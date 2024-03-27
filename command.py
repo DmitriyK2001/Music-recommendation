b3c5b228f408328f1f6340db51c93b11f3ec9988
DmitriyK2001 <kulish.da@phystech.edu>
2024-03-27 18:52:46 +0300

    upd command.py

--- command.py
+++ command.py
@@ -1,9 +1,7 @@
+import fire
 from music.infer import infer
 from music.train import train
 
-def main():
-  train()
-  infer()
-
 if __name__ == '__main__':
-  main()
+    fire.Fire(train)
+    fire.Fire(infer)
