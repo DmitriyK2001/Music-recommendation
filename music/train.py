222606a4da33a6dff4c0ed1832da40e11576ade5
DmitriyK2001 <kulish.da@phystech.edu>
2024-06-08 12:50:14 +0300

    upd train

--- music/train.py
+++ music/train.py
@@ -1,57 +1,143 @@
-import mlflow
-import pandas as pd
-from scipy import io
-from scipy.sparse import coo_matrix
+# from logger import Logger
+import copy
+import math
 
-from music.data_loader import load_config, preprocess
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset
+from torchsummary import summary
+
+from music.torch_model import nn_model
 
 
-# mlflow.set_tracking_uri('http://localhost:5001')
-mlflow.set_tracking_uri("http://host.docker.internal:5001")
-mlflow.set_experiment("MRS expetiment")
+# config device
+
+
+class TrainDataset(Dataset):
+    def __init__(self):
+        self.x = torch.from_numpy(np.load("Training_Data/train_x.npy").astype(np.float32))
+        self.y = torch.from_numpy(
+            np.load("Training_Data/train_y.npy").reshape(-1, 1).astype(np.float32)
+        )
+        self.n_samples = self.x.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+class TestDataset(Dataset):
+    def __init__(self):
+        self.x = torch.from_numpy(np.load("Training_Data/test_x.npy").astype(np.float32))
+        self.y = torch.from_numpy(
+            np.load("Training_Data/test_y.npy").reshape(-1, 1).astype(np.float32)
+        )
+        self.n_samples = self.x.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+def adjust_learning_rate(optimizer, epoch, learning_rate):
+    """Sets the learning rate to the initial LR decayed by 10 every 12 epochs"""
+    lr = learning_rate * (0.1 ** (epoch // 12))
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+        print(param_group["lr"])
 
 
 def train():
-    with mlflow.start_run():
-        user_song_list_count = preprocess()
-        user_song_list_listen = (
-            user_song_list_count[["user", "listen_count"]]
-            .groupby("user")
-            .sum()
-            .reset_index()
-        )
-        user_song_list_listen.rename(
-            columns={"listen_count": "total_listen_count"}, inplace=True
-        )
-        user_song_list_count_merged = pd.merge(
-            user_song_list_count, user_song_list_listen
-        )
-        user_song_list_count_merged["fractional_play_count"] = (
-            user_song_list_count_merged["listen_count"]
-            / user_song_list_count_merged["total_listen_count"]
-        )
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    num_epochs = 16
+    num_classes = 8
+    batch_size = 128
+    learning_rate = 0.0001
+    trainDataset = TrainDataset()
+    train_loader = DataLoader(
+        dataset=trainDataset, batch_size=batch_size, shuffle=True, num_workers=0
+    )
+    testDataset = TestDataset()
+    test_loader = DataLoader(dataset=testDataset, batch_size=batch_size, shuffle=True)
+    # Initialize model
+    model = nn_model.to(device)
+    summary(model, input_size=(1, 128, 128))
+    # logger = Logger('./logs')
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    total_samples = len(trainDataset)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
+    n_iterations = math.ceil(total_samples / batch_size)
+    # Train the model
+    best_accuracy = 0
+    model.train()  # Set model to train mode
+    for epoch in range(num_epochs):
+        adjust_learning_rate(optimizer, epoch, learning_rate)
+        for i, (images, labels) in enumerate(train_loader):
+            model.train()
+            images = (images / 255.0).reshape(-1, 1, 128, 128).to(device)
+            labels = (
+                F.one_hot(labels.type(torch.LongTensor), num_classes)
+                .reshape(-1, num_classes)
+                .to(device)
+            )
 
-        user_codes = user_song_list_count_merged.user.drop_duplicates().reset_index()
-        user_codes.rename(columns={"index": "user_index"}, inplace=True)
-        user_codes["us_index_value"] = list(user_codes.index)
+            # Forward pass
+            outputs = model(images)
 
-        song_codes = user_song_list_count_merged.song.drop_duplicates().reset_index()
-        song_codes.rename(columns={"index": "song_index"}, inplace=True)
-        song_codes["so_index_value"] = list(song_codes.index)
+            loss = criterion(outputs, torch.max(labels, 1)[1])
 
-        small_set = pd.merge(user_song_list_count_merged, song_codes, how="left")
-        small_set = pd.merge(small_set, user_codes, how="left")
-        mat_candidate = small_set[
-            ["us_index_value", "so_index_value", "fractional_play_count"]
-        ]
+            # Backward and optimize
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
 
-        data_array = mat_candidate.fractional_play_count.values
-        row_array = mat_candidate.us_index_value.values
-        col_array = mat_candidate.so_index_value.values
+            # Compute accuracy
+            _, argmax = torch.max(outputs, 1)
+            n = argmax.size(0)
+            accuracy = (torch.max(labels, 1)[1] == argmax).sum().item() / n
 
-        data_sparse = coo_matrix((data_array, (row_array, col_array)), dtype=float)
-        io.hb_write(load_config("train_infer")["state_matrix_file"], data_sparse)
-        small_set.to_csv(load_config("train_infer")["small_set_file"], index=False)
-        mlflow.log_param("my", "param")
-        mlflow.log_metric("score", 100)
-        return data_sparse
+            if accuracy > best_accuracy:
+                best_model = copy.deepcopy(model)
+
+            if (i + 1) % 100 == 0:
+                print(
+                    f"epoch: {epoch+1}/{num_epochs}, step: {i+1}/{n_iterations}, loss: {loss.item():.4f}, accuracy: {accuracy:.4f}"
+                )
+                # print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+                # ================================================================== #
+                #                        Tensorboard Logging                         #
+                # ================================================================== #
+
+                # place for logs
+                # 1. Log scalar values (scalar summary)
+                # 2. Log values and gradients of the parameters (histogram summary)
+                # 3. Log training images (image summary)
+
+    # Test the model
+    model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = (images / 255.0).reshape(-1, 1, 128, 128).to(device)
+            labels = (
+                F.one_hot(labels.type(torch.LongTensor), num_classes)
+                .reshape(-1, num_classes)
+                .to(device)
+            )
+            out = model(images)
+            _, predicted = torch.max(out.data, 1)
+            total += labels.size(0)
+            correct += (predicted == torch.max(labels, 1)[1]).sum().item()
+
+        print("Test Accuracy: {:.4f} %".format(100 * correct / total))
+
+    # Save the best model checkpoint
+    torch.save(best_model.state_dict(), "model.pt")
